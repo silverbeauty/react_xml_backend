4ba5f2d971006de7722f7d06ae6b171c2c8061cf
superwebtop <superwebtop@outlook.com>
2019-04-28 06:34:37 -0700

    urls config

---
+++ constants/urls.js
@@ -0,0 +1,15 @@
+if(process.env.NODE_ENV === 'production') {
+    urls = {
+      DOMAIN_URL: 'http://vizyul.com/',
+      API_URL: 'http://vizyul.com/api/v1',
+      FILE_URL: 'http://vizul.com/api/v1/file/'
+    }
+  } else {
+    urls = {
+      DOMAIN_URL: 'http://localhost:8008/',
+      API_URL: 'http://localhost:3000/api/vi/',
+      FILE_URL: 'http://localhost:3000/api/vi/file/'
+    }
+  }
+
+module.exports = urls
--- controllers/file.js
+++ controllers/file.js
@@ -1,9 +1,9 @@
 const File = require('../models/file')
+const urls = require('../constants/urls')
 
 const create = async (req, res) => {
     if (req.file) {
-        if (req.currentUser) {
-            
+        if (req.currentUser) {     
             const file = new File({
                 user: req.currentUser.id,
                 name: req.file.filename,
@@ -15,7 +15,7 @@ const create = async (req, res) => {
           status: true,
           data: {
             file_name: req.file.filename,
-            url: process.env.VIZYUL_DOMAIN + '/api/file/' + req.file.filename
+            url: urls.FILE_URL + req.file.filename
           }
         })
     }
